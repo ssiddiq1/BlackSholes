964030e217cd65f22f5df77b59cf60aac561e8d7
ssiddiq1 <ssiddiq1@oberlin.edu>
2025-03-25 21:06:15 +0000

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ class BlackScholes:
 with st.sidebar:
     st.title("📊 Black-Scholes Model")
     st.write("`Created by:`")
-    linkedin_url = "https://www.linkedin.com/in/mprudhvi/"
+    linkedin_url = "https://www.linkedin.com/in/shayaan-siddique-981978326/"
     st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Prudhvi Reddy, Muppala`</a>', unsafe_allow_html=True)
 
     current_price = st.number_input("Current Asset Price", value=100.0)
